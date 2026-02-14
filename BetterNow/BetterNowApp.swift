b2cb48a1e32a4e4f6fa6580d3be0eb93e249d9d4
div-antk <51283468+owaii@users.noreply.github.com>
2026-02-14 23:53:45 +0900

    Initial Commit

---
+++ BetterNow/BetterNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterNowApp.swift
+//  BetterNow
+//
+//  Created by Takuya Ando on 2026/02/14.
+//
+
+import SwiftUI
+
+@main
+struct BetterNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterNow
+//
+//  Created by Takuya Ando on 2026/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
